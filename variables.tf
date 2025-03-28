15e8065e000ddd363263de172c44a0412fd978a2
suresh-offcl <sureshgopikhanishk@gmail.com>
2025-03-28 20:57:20 +0530

    add

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "project_name" {
+
+}
+
+variable "environment" {
+
+}
+
+variable "sg_name" {
+
+}
+
+variable "common_tags" {
+    default = {}
+}
+
+variable "sg_tags" {
+  type    = map(string)
+  default = {}
+}
+
+
+variable "vpc_id" {
+  default = {}
+}
